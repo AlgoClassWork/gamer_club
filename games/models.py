58b30006a973a5cfd1d92444209bd6df576670fe
AlgoClassWork <kyxec.play@gmail.com>
2025-11-29 10:30:32 +0600

    Add files via upload

--- games/models.py
+++ games/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField('Название', max_length=200)
+    genre = models.CharField('Жанр', max_length=100)
+    release_year = models.PositiveIntegerField('Год название')
+    description = models.TextField('Описание', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('game_detail', kwargs={'id': self.id})
