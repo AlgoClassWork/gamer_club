a2d2b96b7225a6d7f8ca2043fb78feb2a934d083
AlgoClassWork <kyxec.play@gmail.com>
2025-12-07 07:11:08 +0300

    Add files via upload

--- games/urls.py
+++ games/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
    path('', GameListView.as_view(), name='game_list'),
    path('game/<int:pk>/', GameDetailView.as_view(), name='game_detail'),
-   path('game/new/', GameCreateView.as_view(), name='game_create')
+   path('game/new/', GameCreateView.as_view(), name='game_create'),
+   path('game/update/<int:pk>/', GameUpdateView.as_view(), name='game_update'),
+   path('game/delete/<int:pk>/', GameDeleteView.as_view(), name='game_delete'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,11 +1,12 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Game
 
 class GameListView(ListView):
     model = Game
     template_name = 'game_list.html'
     context_object_name = 'games'
-    paginate_by = 2
+    paginate_by = 5
 
 class GameDetailView(DetailView):
     model = Game
@@ -16,3 +17,13 @@ class GameCreateView(CreateView):
     model = Game
     template_name = 'game_form.html'
     fields = ['title', 'genre', 'release_year', 'description']
+
+class GameUpdateView(UpdateView):
+    model = Game
+    template_name = 'game_form.html'
+    fields = ['title', 'genre', 'release_year', 'description']
+
+class GameDeleteView(DeleteView):
+    model = Game
+    template_name = 'game_confirm_delete.html'
+    success_url = reverse_lazy('game_list')
